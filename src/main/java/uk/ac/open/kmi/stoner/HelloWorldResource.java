c8b2659296eada9455c8b620098dd04ac6db1a6b
enridaga <enricodaga@gmail.com>
2015-01-16 16:39:49 +0000

    Added logging with SLF4J and Log4j 2

--- src/main/java/uk/ac/open/kmi/stoner/HelloWorldResource.java
+++ src/main/java/uk/ac/open/kmi/stoner/HelloWorldResource.java
@@ -4,15 +4,23 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Created by Luca Panziera on 09/01/15.
  */
 @Path("helloworld")
 @Produces("text/plain")
 public class HelloWorldResource {
+	
+	private static Logger log = LoggerFactory.getLogger(HelloWorldResource.class);
+	
     @GET
     @Produces("text/plain")
     public String getHello() {
+    	log.info("Test Info Message");
+    	log.error("Test Error Message");
         return "Hello World!";
     }
 }
