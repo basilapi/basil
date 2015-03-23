4dbb33e4b9075d3722eddd438e3f6ffef1b24c72
enridaga <enricodaga@gmail.com>
2015-03-23 19:16:36 +0000

    Changed in web.xml and removed license description from bootstrap as it is to be defined

--- src/main/java/uk/ac/open/kmi/basil/swagger/Bootstrap.java
+++ src/main/java/uk/ac/open/kmi/basil/swagger/Bootstrap.java
@@ -22,8 +22,8 @@ public class Bootstrap extends HttpServlet {
                 "Tool for building Web APIs on top of SPARQL endpoints.",
                 "",                  /* TOS URL */
                 "",                            /* Contact */
-                "Apache 2.0",                                     /* license */
-                "http://www.apache.org/licenses/LICENSE-2.0.html" /* license URL */
+                "Undefined",                                     /* license */
+                "" /* license URL */
         );
 
         ConfigFactory.config().setApiInfo(info);
