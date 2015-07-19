9951e6cc776afb108a998e1ff394d282d51b01d0
enridaga <enricodaga@gmail.com>
2015-07-19 12:06:26 +0100

    Just remove warnings

--- basil/src/main/java/uk/ac/open/kmi/basil/core/exceptions/ApiInvocationException.java
+++ basil/src/main/java/uk/ac/open/kmi/basil/core/exceptions/ApiInvocationException.java
@@ -4,7 +4,12 @@ package uk.ac.open.kmi.basil.core.exceptions;
  * Created by Luca Panziera on 15/06/15.
  */
 public class ApiInvocationException extends Exception {
-    public ApiInvocationException(String message) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ApiInvocationException(String message) {
         super(message);
     }
 }
--- basil/src/main/java/uk/ac/open/kmi/basil/core/exceptions/SpecificationParsingException.java
+++ basil/src/main/java/uk/ac/open/kmi/basil/core/exceptions/SpecificationParsingException.java
@@ -4,7 +4,12 @@ package uk.ac.open.kmi.basil.core.exceptions;
  * Created by Luca Panziera on 15/06/15.
  */
 public class SpecificationParsingException extends Exception {
-    public SpecificationParsingException(String message) {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SpecificationParsingException(String message) {
         super(message);
     }
 }
